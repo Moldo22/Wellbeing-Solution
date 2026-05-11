ad11c4ceb80d7e46770a292caf57a201f0c24118
lini-raul <raul.lini@yahoo.ro>
2026-05-11 09:22:34 +0300

    Login and Register APIs

--- Backend/Users/urls.py
+++ Backend/Users/urls.py
@@ -1,8 +1,38 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import EmailTokenObtainPairView, RegisterView, UserListView
 
 urlpatterns = [
-    path("users/<name>", views.hello_there, name="hello_there"),
-    path("", views.home, name="home")
+
+    # LOGIN
+    #   JSON expected for login in the following format:
+    #   {
+    #    "email": "test1@example.com",
+    #    "password": "test12345"
+    #   }
+    path("login/", EmailTokenObtainPairView.as_view(), name="login"), 
+
+    # REFRESH TOKEN
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+
+    # REGISTER
+    # JSON expected for register in the following format:
+    #     {
+    #     "email": "testuser1@example.com",
+    #     "password": "Test12345!",
+    #     "first_name": "Raul",
+    #     "last_name": "Lini",
+    #     "city": "Cluj-Napoca",
+    #     "country": "Romania",
+    #     "bio": "Account bio description.",
+    #     "date_of_birth": "1998-05-12",
+    #     "phone_number": "+40712345678",
+    #     "favorite_sports": ["football", "tennis", "basketball"]
+    #     }
+    path("register/", RegisterView.as_view(), name="register"),
+
+    #url only for test purposes
+    path("all/", UserListView.as_view(), name="user-list"),
 
 ]
